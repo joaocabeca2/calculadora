204954053a5dd64b113209bf3a7ada05b09c640d
João Pedro <joaopedro.df12@gmail.com>
2021-02-05 10:19:50 -0300

    função multiplicação

---
+++ multiplicacao.py
@@ -0,0 +1,7 @@
+def multiplicacao(numero1,numero2):
+    return numero1 * numero2
+
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+
+print(multiplicacao(numero1,numero2))
