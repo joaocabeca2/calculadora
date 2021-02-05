713ca91d3c89434e8f2a5ee15cbb650bf8ee16d2
João Pedro <joaopedro.df12@gmail.com>
2021-02-05 10:17:11 -0300

    finalmente adicionei essa parte

---
+++ adicao.py
@@ -0,0 +1,7 @@
+def adicao (numero1,numero2):
+    return numero1 + numero2
+
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+
+print(adicao(numero1,numero2))
